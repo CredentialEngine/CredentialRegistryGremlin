f9fd5b69a1c0011c53367f577e4dde6cd0a2abaa
Rômulo Saksida <romulo@rsaksida.com>
2018-08-24 10:14:06 -0300

    Adjust config keys

--- src/main/kotlin/org/credentialengine/cer/gremlin/Config.kt
+++ src/main/kotlin/org/credentialengine/cer/gremlin/Config.kt
@@ -4,19 +4,27 @@ import java.lang.Integer.parseInt
 
 class Config {
     val gremlinUsername: String
-        get() = System.getenv("GREMLIN_USERNAME") ?: "credentialregistry"
+        get() = System.getenv("GREMLIN_MASTER_USERNAME") ?: "credentialregistry"
     val gremlinPassword: String
-        get() = System.getenv("GREMLIN_PASSWORD") ?: "8Zc7NJIMFDSrUIv"
+        get() = System.getenv("GREMLIN_MASTER_PASSWORD") ?: "8Zc7NJIMFDSrUIv"
     val gremlinAddress: String
-        get() = System.getenv("GREMLIN_ADDRESS") ?: "localhost"
+        get() = System.getenv("GREMLIN_MASTER_ADDRESS") ?: "localhost"
     val gremlinPort: Int
-        get() = parseInt(System.getenv("GREMLIN_PORT") ?: "8183")
+        get() = parseInt(System.getenv("GREMLIN_MASTER_PORT") ?: "8183")
     val databaseUrl: String
-        get() = System.getenv("DATABASE_URL") ?: "jdbc:postgresql://localhost/metadataregistry_development"
+        get() {
+            val psqlAddress = System.getenv("POSTGRESQL_ADDRESS")
+            val psqlDb = System.getenv("POSTGRESQL_DATABASE")
+            if (psqlAddress != null && psqlDb != null) {
+                return "jdbc:postgresql://$psqlAddress/$psqlDb"
+            } else {
+                return "jdbc:postgresql://localhost/metadataregistry_development"
+            }
+        }
     val databaseUsername: String
-        get() = System.getenv("DATABASE_USERNAME") ?: "rmsaksida"
+        get() = System.getenv("POSTGRESQL_USERNAME") ?: "rmsaksida"
     val databasePassword: String
-        get() = System.getenv("DATABASE_PASSWORD") ?: "rmsaksida"
+        get() = System.getenv("POSTGRESQL_PASSWORD") ?: "rmsaksida"
     val redisUrl: String
         get() = System.getenv("REDIS_URL") ?: "redis://localhost:6379"
 }
